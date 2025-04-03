f5b77a2257cff8a098433785ee6903a5eb11d7b5
Siddu <sidduswamy925@gmail.com>
2025-04-03 23:07:10 +0530

    updated

--- ZippyGo/src/main/java/in/siddu/controller/RegisterController.java
+++ ZippyGo/src/main/java/in/siddu/controller/RegisterController.java
@@ -1,5 +1,8 @@
 package in.siddu.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class RegisterController {
 
 }
--- ZippyGo/src/main/java/in/siddu/entity/Register.java
+++ ZippyGo/src/main/java/in/siddu/entity/Register.java
@@ -1,5 +1,13 @@
 package in.siddu.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Register {
 
+	@Id
+	private Integer id;
 }
